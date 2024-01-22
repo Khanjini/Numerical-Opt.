150df56bd0f4b1d7af670e57339a9cd9637fd428
Khanjini <97660343+Khanjini@users.noreply.github.com>
2024-01-22 13:24:11 +0900

    Create Newton's method 4.m

---
+++ Newton's method 4.m
@@ -0,0 +1,71 @@
+syms f(x,y)
+f(x,y) = (x + 3*y -5).^2 + (3*x + y - 7).^2;
+
+c= 1;
+r =1;
+z = zeros(30,30);
+for i = -30:30;
+    
+    for j = -30:30;
+        z(c,r) = double(f(i,j));
+        r = r +1;
+    end;
+    r= 1;
+    c = c +1;
+end;
+
+figure;surf(z)
+xlabel('x-axis')
+ylabel('y-axis')
+set(gca,'XTickLabel',[-30:10:30],'YTickLabel',[-30:10:30])
+
+df_x = diff(f,x);
+df_y = diff(f,y);
+
+a = 30, b = 30; 
+curr_point = [a,b];
+curr_eval = double(f(curr_point(1),curr_point(2)));
+
+df_f = [double(df_x(a,b)),double(df_y(a,b))];
+mag_dif = ((double(df_x(a,b))).^2).^(1/2) + ((double(df_y(a,b))).^2).^(1/2);
+
+p = -df_f./mag_dif;
+new_point = curr_point + p;
+
+%% Newton's method
+
+h = hessian(f,[x,y]);
+inv_h = inv(h);
+
+% a = curr_point(1);
+% b = curr_point(2);
+% df_f = [double(df_x(a,b)),double(df_y(a,b))]
+m = 0;
+
+while m < 100 & curr_eval > 0.1
+    
+    new_step = double(inv_h*df_f')';
+    
+    new_point = curr_point - new_step;
+    
+    new_eval = double(f(new_point(1),new_point(2)));
+    
+    if new_eval <= curr_eval;
+        
+        curr_point = new_point;
+        curr_eval = new_eval;
+        
+        a = curr_point(1);
+        b = curr_point(2);
+        
+        df_f = [double(df_x(a,b)),double(df_y(a,b))];
+    end
+    
+    m = m + 1;
+    
+    fprintf('%d times \ncurrent result is %d \n',m,curr_eval);
+    
+    
+end;
+
+% m = 2, best solution = [2,1] --> 0
